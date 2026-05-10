31b6585109326f7661db0293496f4918d5e2ed73
UzuiTengen117 <juanca.chaparro.18@gmail.com>
2026-05-10 10:16:13 -0600

    Actualización del navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { Menu } from './components/menu/menu';
 import { ArquitecturaYServicios } from './components/arquitectura-y-servicios/arquitectura-y-servicios';
 import { LenguajeMarcas } from './components/lenguaje-marcas/lenguaje-marcas';
 import { CSS3 } from './components/css3/css3';
+import { Usabilidad } from './components/usabilidad/usabilidad';
+import { JavaScript } from './components/java-script/java-script';
+import { Php } from './components/php/php';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -11,5 +14,8 @@ export const routes: Routes = [
   { path: 'arquitecturayservicios', component: ArquitecturaYServicios },
   { path: 'lenguajedemarcas', component: LenguajeMarcas },
   { path: 'css3', component: CSS3 },
+  { path: 'usabilidad', component: Usabilidad },
+  { path: 'javascript', component: JavaScript },
+  { path: 'php', component: Php },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
